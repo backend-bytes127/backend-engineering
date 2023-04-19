a643fb6fc2b3c873959938f8e6e0cdd86f3b197d
Neeraj Sujan <nrj127@gmail.com>
2023-04-19 22:39:21 +0200

    task management api

---
+++ project-task-manager/Readme.md
@@ -0,0 +1,45 @@
+# Task Management App
+
+This is a simple task management application that allows users to create, update, and delete tasks. The application has a backend API built with Node.js and Express, and a frontend built with React.
+
+## Getting Started
+To get started with the application, clone the repository and follow the steps below:
+
+
+1. Install dependencies: npm install
+2. Start the server: npm start
+
+
+### API Endpoints
+
+The following API endpoints are available:
+
+1. GET /tasks: Retrieve all tasks
+2. GET /tasks/:id: Retrieve a single task by ID
+3. POST /tasks: Create a new task
+4. PUT /tasks/:id: Update an existing task
+5. DELETE /tasks/:id: Delete a task by ID
+
+### Filtering and Sorting
+
+Users can filter and sort tasks by using the completed and sortBy query parameters. For example:
+
+1. GET /tasks?completed=true: Retrieve all completed tasks
+2. GET /tasks?sortBy=createdAt:desc: Retrieve all tasks sorted by creation date in descending order
+
+### Priority
+Users can assign a priority level to each task by using the priority attribute. The available priority levels are low, medium, and high.
+
+### Demo
+
+To test the POST API, execute the following command on your terminal
+
+```
+curl -X POST -H "Content-Type: application/json" -d '{"title": "Task 1", "description": "This is task 1", "completed": false, "priority": "low"}' http://localhost:5000/tasks
+  ```
+
+To test the GET API, execute the following command on your terminal
+
+```
+curl -X GET 'http://localhost:3000/tasks?sortBy=createdAt:desc&completed=true&priority=high'
+```
---
+++ project-task-manager/src/server.ts
@@ -0,0 +1,105 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+  priority: 'low' | 'medium' | 'high';
+  createdAt: number;
+}
+
+let tasks: Task[] = [];
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/tasks', (req: Request, res: Response) => {
+  const { completed, sortBy } = req.query;
+  let filteredTasks = tasks;
+
+  if (completed) {
+    const isCompleted = completed === 'true';
+    filteredTasks = tasks.filter(task => task.completed === isCompleted);
+  }
+
+  if (sortBy) {
+    filteredTasks.sort((a, b) => {
+      if (sortBy === 'createdAt') {
+        return b.createdAt - a.createdAt;
+      } else {
+        if (a.priority === 'high') {
+          return -1;
+        } else if (b.priority === 'high') {
+          return 1;
+        } else if (a.priority === 'medium') {
+          return -1;
+        } else if (b.priority === 'medium') {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    });
+  }
+
+  res.json(filteredTasks);
+});
+
+app.get('/tasks/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const task = tasks.find(task => task.id === Number(id));
+  if (!task) {
+    return res.status(404).json({ message: 'Task not found' });
+  }
+  res.json(task);
+});
+
+app.post('/tasks', (req: Request, res: Response) => {
+  const { title, description, completed, priority } = req.body;
+  const newTask: Task = {
+    id: tasks.length + 1,
+    title,
+    description,
+    completed: completed || false,
+    priority: priority || 'low',
+    createdAt: Date.now(),
+  };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
+app.put('/tasks/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const taskIndex = tasks.findIndex(task => task.id === Number(id));
+  if (taskIndex === -1) {
+    return res.status(404).json({ message: 'Task not found' });
+  }
+  const { title, description, completed, priority } = req.body;
+  const updatedTask = {
+    ...tasks[taskIndex],
+    title: title || tasks[taskIndex].title,
+    description: description || tasks[taskIndex].description,
+    completed: completed || tasks[taskIndex].completed,
+    priority: priority || tasks[taskIndex].priority,
+  };
+  tasks[taskIndex] = updatedTask;
+  res.json(updatedTask);
+});
+
+app.delete('/tasks/:id', (req: Request, res: Response) => {
+  const taskIndex = tasks.findIndex(task => task.id === parseInt(req.params.id));
+  if (taskIndex === -1) {
+    return res.status(404).json({ message: 'Task not found' });
+  }
+  tasks.splice(taskIndex, 1);
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
